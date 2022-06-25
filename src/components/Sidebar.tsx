1e0c9feaaa561c05f0a16a613fc31bbfe0af1744
Gabriel Polo <gabriel@pumpycode.com>
2022-06-25 17:37:57 -0300

    aula 5

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,30 +1,10 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
-const GET_LESSONS_QUERY = gql`
-query MyQuery {
-  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-    id
-    lessonType
-    availableAt
-    title
-    slug
-  }
-}    
-`
-
-interface GetLEssonsQueryResponse {
-    lessons: {
-        id: string;
-        title: string;
-        slug: string;
-        availableAt: string;
-        lessonType: 'live' | 'class';
-    }[]
-}
 
 export function Sidebar() {
-    const { data } = useQuery<GetLEssonsQueryResponse>(GET_LESSONS_QUERY)
+    const { data } = useGetLessonsQuery();
 
     return (
         <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
